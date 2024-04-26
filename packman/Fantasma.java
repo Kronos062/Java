e06bb017b77e8317cfb6a1c765ed43a0a6e03518
«Kronos062» <0.zawar.k.0@gmail.com>
2024-04-26 10:56:48 +0200

    arreglo fantasmas y creo cada uno de ellos

--- packman/Fantasma.java
+++ packman/Fantasma.java
@@ -1,7 +1,26 @@
 package packman
 
 public class Fantasma {
-    public static void packman() {
-        
+    private int posX;
+    private int posY;
+// creo los fantasmas pero no se como hacer a cada uno con su personalidad.
+    public void Pinky(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void Inky(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void Blinky(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void Clyde(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
     }
 }
