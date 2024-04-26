a3e76fbc1b470cc818ed508f849c53c226669ad5
«Kronos062» <0.zawar.k.0@gmail.com>
2024-04-26 10:40:22 +0200

    creo lo opcional con mensajes

---
+++ packman/otro.java
@@ -0,0 +1,22 @@
+package packman
+
+public class otro{
+    public static void bola(String[] args){
+        System.out.println("soy una bola");
+    }
+    public static void bolaEspecial(String[] args) {
+        System.out.println("soy una bola especial");
+    }
+    public static void fruta(String[] args) {
+        System.out.println("soy una fruta");
+    }
+    public static void laberinto(String[] args) {
+        System.out.println("Quiero que sea un matriz para que se mueva");
+    }
+    public static void pasadizo() {
+        System.out.println("quiero declarar nulos en la matriz para que cuando packman vea un nulo no pueda pasar");
+    }
+    public static void casaFantasamas(String[] args) {
+        System.out.println("Casa de fantasamas");
+    }
+}
