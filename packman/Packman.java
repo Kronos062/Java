fadeed4f4f97c7e137dde541d170c95a59208bfb
«Kronos062» <0.zawar.k.0@gmail.com>
2024-04-26 10:33:46 +0200

    subo los archivos junto lo que he pegado del enunciado

---
+++ packman/Packman.java
@@ -0,0 +1,10 @@
+package packman
+//primero cremoas la clase:
+public class Packman {    
+    move(int newPosX, int newPosY)
+    pickupBola()
+    pickupBolaEspecial()
+    pickupFruita()
+    loseLife()
+    detectColisionFantasma(Fantasma fantasma)
+}
