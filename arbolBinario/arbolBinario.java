e8f34a0d7822854dfbeac8f9c68983c236fbdb54
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-03 10:35:04 +0200

    creo el class main y hago una pequeña estructura

---
+++ arbolBinario/arbolBinario.java
@@ -0,0 +1,9 @@
+public class arbolBinario {
+    // aqui tengo que poner los tipos de datos:
+
+    //creo arbol binario:
+    public arbolBinario()
+
+    /*3. Volem que les instancies d’arbre sapiguen quantes instancies coexisteixen a la vegada.
+    esto de arriba lo hare con un contador*/
+    //se llamara contador arbol y solo hara un return del contador.
