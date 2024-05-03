fc37c1e6ab6e5548e5c16253bee4f5c8352189dc
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-03 11:39:39 +0200

    cambio arbol ya que se usara NODO para los movimientos

--- arbolBinario/arbolBinario.java
+++ arbolBinario/arbolBinario.java
@@ -1,14 +1,10 @@
 package arbolBinario;
 //1. Crea una nova classe en Java per a la implementació de l'arbre binari:
 public class arbolBinario {
-    // aqui tengo que poner los tipos de datos:
-    // no sabia crear un contador, pero es igual que en python (https://es.stackoverflow.com/questions/468849/utilizar-un-contador-haciendo-recursi%C3%B3n-en-java)
-    int contadorArbol = 0;
-    // el arbol solo puede ir para dos lados, izquierda y derecha (como un personaje de mario bros)
-    // con un "=" me sale error, pero asi no.
-    arbolBinario izquierda;
-    arbolBinario derecha;
-    //creo arbol binario:
+    //no ha servdio de nada lo que he hecho, creo nodo, profundidad y dejo el contador (lo pongo como estatico e int para los errores):
+    Nodo raiz;
+    int profundidad;
+    static int contadorArbol = 0;
     //2. Implementa constructors per a la creació de l'arbre binari:
     public arbolBinario() {
         this.izquierda = null;
--- arbolBinario/nodo.java
+++ arbolBinario/nodo.java
@@ -1,5 +1,7 @@
 package arbolBinario;
-
+//en el nodo
 public class nodo {
-    
+    Nodo izquierda;
+    Nodo derecha;
+    int profundidad;
 }
