aa08062758ee0d579853134a899b3e77d4ba252e
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-03 10:42:56 +0200

    creo que algo eesta mal, pero voy avanzando

--- arbolBinario/arbolBinario.java
+++ arbolBinario/arbolBinario.java
@@ -1,9 +1,21 @@
 public class arbolBinario {
     // aqui tengo que poner los tipos de datos:
-
+    // no sabia crear un contador, pero es igual que en python (https://es.stackoverflow.com/questions/468849/utilizar-un-contador-haciendo-recursi%C3%B3n-en-java)
+    int contadorArbol = 0;
+    // el arbol solo puede ir para dos lados, izquierda y derecha (como un personaje de mario bros)
+    // con un "=" me sale error, pero asi no.
+    arbolBinario izquierda;
+    arbolBinario derecha;
     //creo arbol binario:
-    public arbolBinario()
-
+    public arbolBinario() {
+        this.izquierda = null;
+        this.derecha = null;
+        contadorArbol++;
+    }
     /*3. Volem que les instancies d’arbre sapiguen quantes instancies coexisteixen a la vegada.
     esto de arriba lo hare con un contador*/
     //se llamara contador arbol y solo hara un return del contador.
+    public static int getContadorArbol() {
+        return contadorArbol;
+    }
+}
