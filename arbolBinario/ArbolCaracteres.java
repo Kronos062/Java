140d23aaa0321f1a211885b6592cb5edbb526652
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-03 12:36:46 +0200

    jaja, comí pito

--- arbolBinario/ArbolCaracteres.java
+++ arbolBinario/ArbolCaracteres.java
@@ -1,7 +1,7 @@
 package arbolBinario;
 
     // esto es lo mismo de arbolEnteros pero con characters.
-    public class ArbolCaracteres extends ArbolBinario<Character> {
+    public class ArbolCaracteres extends arbolBinario.ArbolCaracteres<Character> {
         public ArbolCaracteres(int profundidad) {
             super(profundidad);
 
