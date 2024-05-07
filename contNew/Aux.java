a4347e954b694e677ff6223efc01b0d503c806e3
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-07 12:20:14 +0200

    si

---
+++ contNew/Aux.java
@@ -0,0 +1,26 @@
+package contNew;
+
+class Aux 
+{
+    public static int num_instancies = 0;
+
+    public Aux() {
+            num_instancies++;
+    }
+    public static int getNumInstances() {
+        return num_instancies;
+    }
+}
+
+class Main 
+{
+    public static void main(String[] args) {
+        System.out.println("Numero de instancias inicial:" + Aux.getNumInstances());
+        Aux x = new Aux();
+        System.out.println("Numero de instancias: " + x.getNumInstances());
+        System.out.println("Numero de instancias (Usando aux): " + Aux.getNumInstances());
+        Aux y = new Aux();
+        System.out.println("Numero de instancias: " + x.getNumInstances());
+        System.out.println("Numero de instancias (Usando aux): " + Aux.getNumInstances());
+    }
+}
