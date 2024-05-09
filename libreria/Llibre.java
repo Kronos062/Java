ee10389bc188495a9de5a9d1ee32f44f8b55f6ea
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-09 10:54:12 +0200

    creo clases y empiezo Llibre

---
+++ libreria/Llibre.java
@@ -0,0 +1,33 @@
+package libreria;
+
+public class Llibre {
+    private String titol;
+
+    public Llibre(String titol) {
+        this.titol = titol;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+}
+
+class LlistaLlibres<T> {
+
+}
+
+public class Main {
+    
+}
+
+class Novela {
+    
+}
+
+class CienciaFiccio {
+    
+}
+
+class Poesia {
+    
+}
