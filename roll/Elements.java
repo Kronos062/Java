4f7f41aacc93b5e6d4675b7eb79a300a6e5073e0
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-10 11:43:43 +0200

    estoy haciedo armadura y arma para poder hacer personage

--- roll/Elements.java
+++ roll/Elements.java
@@ -1,5 +1,42 @@
 package roll;
 
 public class Elements {
+    public class Arma {
+        private String nom;
+        private int danys;
     
+        // Constructor
+        public Arma(String nom, int danys) {
+            this.nom = nom;
+            this.danys = danys;
+        }
+    //nombre del arma
+        public String getNom() {
+            return nom;
+        }
+    //poner nombre al arma
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+    //el daño que hace
+        public int getDanys() {
+            return danys;
+        }
+    //poner el daño que hace
+        public void setDanys(int danys) {
+            this.danys = danys;
+        }
+    }
+    //ahora creo la clase armadura
+    public class Armadura {
+        private String nom;
+        private int resistència;
+
+        // Constructor
+        public Armadura(String nom, int resistència) {
+            this.nom = nom;
+            this.resistència = resistència;
+        }
+    }
 }
+
--- roll/Personatge.java
+++ roll/Personatge.java
@@ -1,5 +1,17 @@
 package roll;
 
 public class Personatge {
-    
+        private String nom;
+        private int nivell;
+        private int puntsDeVida;
+        private int puntsDeMana;
+        // Constructor
+    public Personatge(String nom, int nivell, int puntsDeVida, int puntsDeMana, Arma arma, Armadura armadura) {
+        this.nom = nom;
+        this.nivell = nivell;
+        this.puntsDeVida = puntsDeVida;
+        this.puntsDeMana = puntsDeMana;
+        this.arma = arma;
+        this.armadura = armadura;
+    }
 }
