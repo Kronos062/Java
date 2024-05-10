022a904f03a20e3cce2e2fdb654c3f95c6fd31e0
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-10 10:46:31 +0200

    esty haciendo lo que puedo con importar personages (escrbo en castellano pero el xml esta en catalan)

--- roll/ImportarXml.java
+++ roll/ImportarXml.java
@@ -4,50 +4,26 @@ import org.w3c.dom.*;
 import java.io.*;
 
 public class ImportarXml {
-    private static void howToExtractDataFromXML() {
+    private static void importarPersonajes() {
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         Document document = null;
         
         try {
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
-            File f = new File ("elMeuFitxer.xml");
+            File f = new File ("personatges.xml");
             document = builder.parse(f);
             Element e = document.getDocumentElement();
             //Obtenir tots els nodes del document anomenats "nom"
-            NodeList llistaElems = e.getElementsByTagName("Nom");
+            NodeList llistaPersonatge = e.getElementsByTagName("personatge");
             //Recorregut d'elements anomenats "Nom"
-            for (int i = 0; i < llistaElems.getLength(); i++) {
-                Element elem = (Element)llistaElems.item(i);
-                NodeList llistaFills = elem.getChildNodes();
-                //Recorregut de nodes fill d'un element (text, atributs, etc.)
-                String text = "";
-                for (int j = 0; j < llistaFills.getLength(); j++) {
-                    Node n = llistaFills.item(j);
-                    //Mirar si els nodes són de text, com veureu CDATA ho hevist a LMSGI
-                    if ( (n.getNodeType() == Node.TEXT_NODE)||
-                        (n.getNodeType() == Node.CDATA_SECTION_NODE) ) {
-                
-                        text += n.getNodeValue();
-                    }
-                }
-
-                //Arribat aquest punt veureu com aqui podriem extreure el codi per no repetir-nos, queda com a exercici
-                NodeList llistaPreus = ((Element)elem.getParentNode()).getElementsByTagName("Preu");
-                for (int j = 0; j < llistaPreus.getLength(); j++) {
-                    Element preu = (Element)llistaPreus.item(j);
-                    NodeList llistaFillsPreu = preu.getChildNodes();
-                    for (int k = 0; k < llistaFillsPreu.getLength(); k++) { 
-                        Node p = llistaFillsPreu.item(k);
-                        //Mirar si els nodes són de text, com veureu CDATA ho hevist a LMSGI
-                        if ( (p.getNodeType() == Node.TEXT_NODE)||
-                            (p.getNodeType() == Node.CDATA_SECTION_NODE) ) {
-                    
-                            text += " " + p.getNodeValue();
-                        }
-                    }
-                    System.out.println(text);
-                }
-            }
+            for (int i = 0; i < llistaPersonatge.getLength(); i++) {
+                Element personatge = (Element)llistaPersonatge.item(i);
+                String nom = getTextContent(personatge, "nom")
+                nivell
+                puntsDeVida
+                puntsDeMana
+                arma
+                armadura
         } catch (Exception ex) {
             System.out.println("Error en la lectura del document: " + ex);
         }
