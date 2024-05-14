e7f469c285051cee146bf05fd45c7340839f5152
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-14 14:33:40 +0200

    hazlo perro

---
+++ GUI/Sin.java
@@ -0,0 +1,12 @@
+import java.awt.Button;
+import java.awt.Frame;
+
+public class Sin {
+    public static void main(String[] args){
+        Frame frame = new Frame("ventanica"); 
+        Button boton = new Button("Clicame");
+            boton.setVisible(true);
+            frame.setVisible(true);
+            frame.setSize(300, 200);
+    }
+}
