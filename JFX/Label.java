ca788697729fd22109c3e1767ae400a2894ea17b
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-16 10:45:43 +0200

    claro que si

---
+++ JFX/Label.java
@@ -0,0 +1,9 @@
+package JFX;
+
+public class Label {
+
+    public Label(String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
---
+++ JFX/Scene.java
@@ -0,0 +1,9 @@
+package JFX;
+
+public class Scene {
+
+    public Scene(StackPane stackPane, int i, int j) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
