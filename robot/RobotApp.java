2936ec42bf854cea9ececf7291da452ef6a6161d
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-24 10:41:04 +0200

    hago un peuqueño esquema de como sera lo que haya en pantalla y lo que tiene que hacer en cada una de sus funciones.

--- robot/RobotApp.java
+++ robot/RobotApp.java
@@ -8,16 +8,49 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class RobotApp extends Application{
+    private Robot robot;
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Aplicación de Salu2");
 
-        Button btnSalutacio = new Button();
-        btnSalutacio.setText("Clica aquí");
-        btnSalutacio.setOnAction(e -> {
-            mostrarMensageSalu2();
+        robot = new Robot();
+        Button moverAdelanteButton = new Button("Mover adelante");
+        Button moverAtrasButton = new Button("Mover atras");
+        Button girarDerechaButton = new Button("Girar derecha");
+        Button girarIzquierdaButton = new Button("Girar izquierda");
+        Button guardarButton = new Button("Guardar estado");
+        Button cargarButton = new Button("Cargar estado");
+
+        moverAdelanteButton.setOnAction(e -> {
+            robot.moverAdelante();
+            return("Se ha movido hacia adelante\n");
         });
+
+        moverAtrasButton.setOnAction(e -> {
+            robot.moverAtras();
+            return("Se ha movido hacia atras\n");
+        });
+
+        girarDerechaButton.setOnAction(e -> {
+            robot.girarDerecha();
+            return("Se ha movido hacia la derecha\n");
+        });
+
+        girarIzquierdaButton.setOnAction(e -> {
+            robot.girarIzquierda();
+            return("Se ha movido hacia la izquierda\n");
+        });
+
+        guardarButton.setOnAction(e -> {
+            robot.guardar();
+            return("Se ha guardado\n");
+        });
+        cargarButton.setOnAction(e -> {
+            robot.cargar();
+            return("Se ha cargado.\n");
+        });
+
 
         StackPane root = new StackPane();
         root.getChildren().add(btnSalutacio);
