1ac718282b2e0d49592e821c717fcb97b32bab07
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-30 10:24:52 +0200

    empuezo con mi gestor de base de datos

---
+++ GestorLibreria/GestorBD.java
@@ -0,0 +1,12 @@
+package GestorLibreria;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorBD {
+    //empiezo creando como logear y donde esta situada la base de datos
+    private static final String URL = "pues no se cual es";
+    private static final String USER = "root";
+    private static final String PASSWORD = "a";
+}
