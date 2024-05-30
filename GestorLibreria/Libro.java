9a2d19784b29f12d4a607cb4f8288e061f3e82fa
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-30 10:20:25 +0200

    no he hecho commits, he creado la estructura para libro

---
+++ GestorLibreria/Libro.java
@@ -0,0 +1,21 @@
+public class Libro {
+    //declaro de que va a estar compuesto libro
+    private int id;
+    private String titulo;
+    private String autor;
+    private int año;
+    //Creo "libro"
+    public Libro(int id, String titulo, String autor, int año) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.año = año;
+    }
+    //voy a hacer los get y los set
+    public int getId() { retiurn id; }
+    public String getTilulo() {return titulo; }
+    public String getAutor() { return autor;}
+    public int getAño() { return año; }
+
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+}
