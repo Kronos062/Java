b9a44f21b7356b456aef8ed712437e91844a7f27
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-30 10:28:41 +0200

    empiezo el añadir libro de mi gestor

--- GestorLibreria/GestorBD.java
+++ GestorLibreria/GestorBD.java
@@ -9,4 +9,12 @@ public class GestorBD {
     private static final String URL = "pues no se cual es";
     private static final String USER = "root";
     private static final String PASSWORD = "a";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void añadirLibro(Libro libro) {
+        String sql = "INSERT INTO libros (titulo, autor, año) Values (?, ?, ?)"
+    }
 }
