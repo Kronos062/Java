0b458c9183bac3aa5bee5a9e4afdfbf83b9ab717
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-30 10:51:39 +0200

    encuentro el link

--- GestorLibreria/GestorBD.java
+++ GestorLibreria/GestorBD.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class GestorBD {
     //empiezo creando como logear y donde esta situada la base de datos
-    private static final String URL = "pues no se cual es";
+    private static final String URL = "jdbc:mysql://localhost:3306/libreria";
     private static final String USER = "root";
     private static final String PASSWORD = "a";
 
--- GestorLibreria/Main.java
+++ GestorLibreria/Main.java
@@ -1,7 +1,36 @@
 package GestorLibreria;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+            //ingresar uin libro
+        System.out.print("Ingrese el ID del libro: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Ingrese el título del libro: ");
+        String titulo = scanner.nextLine();
+        System.out.print("Ingrese el autor del libro: ");
+        String autor = scanner.nextLine();
+        System.out.print("Ingrese el año del libro: ");
+        int año = scanner.nextInt();
+        //creo libro con lo que pone el user
+        Libro libro = new Libro(id, titulo, autor, año);
+        GestorBD.añadirLibro(libro);
+        System.out.println("Libro añadido exitosamente.");
+
+        // para actualizar el libro
+        System.out.print("Ingrese el nuevo título del libro: ");
+        String nuevoTitulo = scanner.nextLine();
+        libro.setTitulo(nuevoTitulo);
+        GestorBD.actualizarLibro(libro);
+
+        System.out.println("Libro actualizado exitosamente.");
+
+        //eliminar libro
+        GestorBD.eliminarLibro(id);
+        System.out.println("Libro eliminado exitosamente.");
     }
 }
