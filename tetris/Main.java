8fdf017909757dcb24f569e50b24fc8ec3e3455d
«Kronos062» <0.zawar.k.0@gmail.com>
2024-05-31 12:17:31 +0200

    OSTIA, que no he cambiado los imports

--- tetris/Main.java
+++ tetris/Main.java
@@ -1,9 +1,10 @@
 package tetris;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
